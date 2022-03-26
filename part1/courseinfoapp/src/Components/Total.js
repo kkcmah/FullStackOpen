61719657591fc44c9ef226b1eab755105393d63c
Kelby Mah <kelbymah@gmail.com>
2022-03-26 10:46:47 -0600

    exercise 1.4 course info step 4 refactor App component objects into an array

--- part1/courseinfoapp/src/Components/Total.js
+++ part1/courseinfoapp/src/Components/Total.js
@@ -1,5 +1,6 @@
 const Total = (props) => {
-  return <p>Number of exercises {props.total}</p>;
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+  return <p>Number of exercises {total}</p>;
 };
 
 export default Total;
