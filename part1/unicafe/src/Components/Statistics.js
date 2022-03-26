1ca29494ef69d7fb006f336e6a40fa5f77837149
Kelby Mah <kelbymah@gmail.com>
2022-03-26 12:17:39 -0600

    exercise 1.7 1.8 unicafe step 2 step 3 feedback statistics

--- part1/unicafe/src/Components/Statistics.js
+++ part1/unicafe/src/Components/Statistics.js
@@ -1,12 +1,18 @@
 import StatisticLine from "./StatisticLine";
 
-const Statistics = ({good, neutral, bad}) => {
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = good / all + "%";
   return (
     <>
       <h1>Statistics</h1>
       <StatisticLine text="good" value={good} />
       <StatisticLine text="neutral" value={neutral} />
       <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} />
     </>
   );
 };
