62aa8e07c6dd0e4ebd3493e446c58b7ec90cea04
Kelby Mah <kelbymah@gmail.com>
2022-03-29 20:59:32 -0600

    exercise 4.2 blog list step 2 refactor blog router into its own module

--- part4/bloglistBackend/index.js
+++ part4/bloglistBackend/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const blogRouter = require("./controllers/blogs")
 
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl);
@@ -12,19 +12,7 @@ mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use("/api/blogs", blogRouter)
 
 const PORT = 3003;
 app.listen(PORT, () => {
