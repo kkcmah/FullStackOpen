ebb860b317193ea4d802084ba19d8a9fde7f63da
Kelby Mah <kelbymah@gmail.com>
2022-03-30 19:38:41 -0600

    exercise 4.4 helper functions and unit tests step 2 add totallikes function and related tests

--- part4/bloglistBackend/utils/list_helper.js
+++ part4/bloglistBackend/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0;
+  return blogs.reduce((prev, cur) => prev + cur.likes, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
