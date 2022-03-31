02e5bd2fcb013d119955fba6ff9728b41a8fa5cc
Kelby Mah <kelbymah@gmail.com>
2022-03-30 19:51:36 -0600

    exercise 4.5 helper functions and unit tests step 3 add function for favorite blog and tests

--- part4/bloglistBackend/tests/list_helper.test.js
+++ part4/bloglistBackend/tests/list_helper.test.js
@@ -84,3 +84,45 @@ describe("total likes", () => {
     expect(result).toBe(36);
   });
 });
+
+describe("favorite blog", () => {
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  test("when list has only one blog, favorite blog is the one blog", () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    expect(result).toEqual({
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    });
+  });
+
+  test("when list has no blogs favorite blog is empty", () => {
+    const result = listHelper.favoriteBlog([]);
+    expect(result).toEqual({});
+  });
+
+  test("when list has many blogs favorite blog is one with most likes", () => {
+    const result = listHelper.favoriteBlog(blogs);
+    expect(result).toEqual({
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    });
+  });
+});
--- part4/bloglistBackend/utils/list_helper.js
+++ part4/bloglistBackend/utils/list_helper.js
@@ -7,7 +7,14 @@ const totalLikes = (blogs) => {
   return blogs.reduce((prev, cur) => prev + cur.likes, 0);
 };
 
+// returns blog with most likes
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {};
+  return blogs.reduce((prev, cur) => (prev.likes > cur.likes ? prev : cur));
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
