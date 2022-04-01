8d2fedb2bd5c1c47e3aa4bea42be770b0e311536
Kelby Mah <kelbymah@gmail.com>
2022-04-01 13:59:28 -0600

    exercise 4.15 bloglist expansion step 3 implement HTTP POST to create user with pass hashing using bcrypt

--- part4/bloglistBackend/app.js
+++ part4/bloglistBackend/app.js
@@ -6,6 +6,7 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const blogRouter = require("./controllers/blogs");
+const userRouter = require("./controllers/users");
 const middleware =require("./utils/middleware")
 
 mongoose.connect(config.MONGODB_URI);
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.static("build"));
 app.use(express.json());
 
+app.use("/api/users", userRouter);
 app.use("/api/blogs", blogRouter);
 
 app.use(middleware.unknownEndpoint)
--- part4/bloglistBackend/index.js
+++ part4/bloglistBackend/index.js
@@ -1,3 +1,4 @@
+const config = require("./utils/config");
 const app = require("./app")
 const http = require("http");
 
