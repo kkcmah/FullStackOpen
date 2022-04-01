27762fd6ca4932346362b4a47e1d56e725feeb5c
Kelby Mah <kelbymah@gmail.com>
2022-03-31 21:21:04 -0600

    exercise 4.10 blog list tests step 3 test HTTP POST to /api/blogs adds blog and its contents are correct

--- part4/bloglistBackend/tests/blog_api.test.js
+++ part4/bloglistBackend/tests/blog_api.test.js
@@ -31,6 +31,40 @@ test("unique identifier property of the blog posts is named id", async () => {
   expect(response.body[0].id).toBeDefined();
 });
 
+test("a valid blog can be added", async () => {
+  const newBlog = {
+    title: "new title",
+    author: "new author",
+    url: "new url",
+    likes: 5,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/blogs");
+
+  const updatedblogs = response.body.map((r) => {
+    return {
+      title: r.title,
+      author: r.author,
+      url: r.url,
+      likes: r.likes,
+    };
+  });
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1);
+  expect(updatedblogs).toContainEqual({
+    title: "new title",
+    author: "new author",
+    url: "new url",
+    likes: 5,
+  });
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
