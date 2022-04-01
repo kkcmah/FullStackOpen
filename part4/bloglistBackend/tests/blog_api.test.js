2c201034d6bdfdd8c5fa85935990d574902dc476
Kelby Mah <kelbymah@gmail.com>
2022-03-31 20:54:39 -0600

    exercise 4.8 blog list tests step 1 testing HTTP GET to /api/blogs verify blog length is expected and type is json

--- part4/bloglistBackend/tests/blog_api.test.js
+++ part4/bloglistBackend/tests/blog_api.test.js
@@ -1,16 +1,31 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const api = supertest(app);
+const Blog = require("../models/blog");
+const helper = require("./test_helper");
 
-const api = supertest(app)
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+});
 
-test('notes are returned as json', async () => {
+test("blogs are returned as json", async () => {
   await api
-    .get('/api/blogs')
+    .get("/api/blogs")
     .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+    .expect("Content-Type", /application\/json/);
+});
+
+test(`there are ${helper.initialBlogs.length} blogs`, async () => {
+  const response = await api.get("/api/blogs");
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
+});
 
 afterAll(() => {
-  mongoose.connection.close()
-})
+  mongoose.connection.close();
+});
