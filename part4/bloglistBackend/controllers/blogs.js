5ece3a29165d391725e851fb58411946e5737f32
Kelby Mah <kelbymah@gmail.com>
2022-03-31 21:29:02 -0600

    exercise 4.11 blog list tests step 4 adding blog missing likes property defaults value to 0 and test

--- part4/bloglistBackend/controllers/blogs.js
+++ part4/bloglistBackend/controllers/blogs.js
@@ -2,12 +2,15 @@ const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
 blogRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({});
   response.json(blogs);
 });
 
 blogRouter.post("/", async (request, response) => {
   const blog = new Blog(request.body);
+  if (blog.likes === undefined) {
+    blog.likes = 0;
+  }
 
   const result = await blog.save();
   response.status(201).json(result);
--- part4/bloglistBackend/tests/blog_api.test.js
+++ part4/bloglistBackend/tests/blog_api.test.js
@@ -65,6 +65,39 @@ test("a valid blog can be added", async () => {
   });
 });
 
+test("adding a blog missing the 'likes' property defaults 'likes' to 0", async () => {
+    const newBlog = {
+      title: "new title",
+      author: "new author",
+      url: "new url",
+    };
+  
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+  
+    const response = await api.get("/api/blogs");
+  
+    const updatedblogs = response.body.map((r) => {
+      return {
+        title: r.title,
+        author: r.author,
+        url: r.url,
+        likes: r.likes,
+      };
+    });
+  
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1);
+    expect(updatedblogs).toContainEqual({
+      title: "new title",
+      author: "new author",
+      url: "new url",
+      likes: 0,
+    });
+  });
+
 afterAll(() => {
   mongoose.connection.close();
 });
