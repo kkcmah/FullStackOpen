0dc96d6836f2b51eb825c9dc87469693192c9b34
Kelby Mah <kelbymah@gmail.com>
2022-03-31 20:29:16 -0600

    exercise 4.8 blog list tests step 1 refactor app setup into its own module and start setting up test environment

--- part4/bloglistBackend/index.js
+++ part4/bloglistBackend/index.js
@@ -1,17 +1,5 @@
-const config = require("./utils/config")
+const app = require("./app")
 const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-const blogRouter = require("./controllers/blogs")
-
-mongoose.connect(config.MONGODB_URI);
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/blogs", blogRouter)
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
