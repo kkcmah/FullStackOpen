0148ed98cea36f108a83e991cc96ef7788aef6c2
Kelby Mah <kelbymah@gmail.com>
2022-04-01 20:15:27 -0600

    exercise 4.19 bloglist expansion step 7 adding new blog only possible using valid token which identifies user

--- part4/bloglistBackend/controllers/blogs.js
+++ part4/bloglistBackend/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogRouter = require("express").Router();
+const jwt = require('jsonwebtoken')
 const Blog = require("../models/blog");
 const User = require("../models/user");
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", {
@@ -13,6 +22,13 @@ blogRouter.get("/", async (request, response) => {
 
 blogRouter.post("/", async (request, response) => {
   const body = new Blog(request.body);
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  
   if (body.likes === undefined) {
     body.likes = 0;
   }
@@ -21,8 +37,7 @@ blogRouter.post("/", async (request, response) => {
     return response.status(400).end();
   }
 
-  // get random user and assign as blog creator
-  const user = await User.findOne({});
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
