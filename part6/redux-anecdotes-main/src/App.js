3790064ac9604cee0a0a0eb466907b1f708bdcac
Kelby Mah <kelbymah@gmail.com>
2022-04-06 13:00:09 -0600

    exercise 6.7 anecdotes step 5 refactor anecdote creation into own component AnecdoteForm

--- part6/redux-anecdotes-main/src/App.js
+++ part6/redux-anecdotes-main/src/App.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
-import { voteAnec, createAnec } from "./reducers/anecdoteReducer";
+import { voteAnec } from "./reducers/anecdoteReducer";
+import AnecdoteForm from "./components/AnecdoteForm";
 
 const App = () => {
   const anecdotes = useSelector((state) => state);
@@ -8,13 +9,6 @@ const App = () => {
 
   const vote = (id) => {
     dispatch(voteAnec(id));
-  };
-
-  const create = (event) => {
-    event.preventDefault();
-    const anecdote = event.target.anecdote.value;
-    event.target.anecdote.value = "";
-    dispatch(createAnec(anecdote));
   };
 
   return (
@@ -30,12 +24,7 @@ const App = () => {
         </div>
       ))}
       <h2>create new</h2>
-      <form onSubmit={create}>
-        <div>
-          <input name="anecdote" />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm></AnecdoteForm>
     </div>
   );
 };
---
+++ part6/redux-anecdotes-main/src/components/AnecdoteForm.js
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { createAnec } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+
+  const create = (event) => {
+    event.preventDefault();
+    const anecdote = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    dispatch(createAnec(anecdote));
+  };
+  return (
+    <form onSubmit={create}>
+      <div>
+        <input name="anecdote" />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  );
+};
+
+export default AnecdoteForm;
