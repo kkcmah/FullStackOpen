e6f8e62d8a2a163b222c84ad0fec420a3f69cbea
Kelby Mah <kelbymah@gmail.com>
2022-04-06 15:00:18 -0600

    exercise 6.10 better anecdotes step 8 extend notification component to render message in redux store

---
+++ part6/redux-anecdotes-main/src/reducers/notificationReducer.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const notificationSlice = createSlice({
+  name: "notification",
+  initialState: "initial notification state",
+  reducers: {},
+});
+
+export default notificationSlice.reducer;
