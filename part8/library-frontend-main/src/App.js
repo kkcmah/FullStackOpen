d74eff83fc0606b30b9b5e595c8320f0363f9ecb
Kelby Mah <kelbymah@gmail.com>
2022-04-10 17:18:57 -0600

    exercise 8.8 authors view implement view to show all authors on a page using apollo client GraphQL hooks

---
+++ part8/library-frontend-main/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { ALL_AUTHORS } from "./queries";
+
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+
+const App = () => {
+  const [page, setPage] = useState("authors");
+
+  const result = useQuery(ALL_AUTHORS)
+
+  if (result.loading) return <div>Loading...</div> 
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage("authors")}>authors</button>
+        <button onClick={() => setPage("books")}>books</button>
+        <button onClick={() => setPage("add")}>add book</button>
+      </div>
+
+      <Authors authors={result.data.allAuthors} show={page === "authors"} />
+
+      <Books show={page === "books"} />
+
+      <NewBook show={page === "add"} />
+    </div>
+  );
+};
+
+export default App;
