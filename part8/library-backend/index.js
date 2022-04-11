09e00287f2c576aa1312d0ac482b467c2155ed1a
Kelby Mah <kelbymah@gmail.com>
2022-04-11 12:00:58 -0600

    exercise 8.16 user and logging in add user management with password as everyone's pass mutation only possible if request includes valid token

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,9 +1,15 @@
-const { ApolloServer, gql, UserInputError } = require("apollo-server");
+const {
+  ApolloServer,
+  gql,
+  UserInputError,
+  AuthenticationError,
+} = require("apollo-server");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 const Book = require("./models/book");
 const Author = require("./models/author");
-const author = require("./models/author");
+const User = require("./models/user");
 
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -33,11 +39,22 @@ const typeDefs = gql`
     id: ID!
   }
 
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    me: User
   }
 
   type Mutation {
@@ -48,6 +65,8 @@ const typeDefs = gql`
       genres: [String!]!
     ): Book!
     editAuthor(name: String!, setBornTo: Int!): Author
+    createUser(username: String!, favoriteGenre: String!): User
+    login(username: String!, password: String!): Token
   }
 `;
 
@@ -64,6 +83,9 @@ const resolvers = {
       return Book.find({}).populate("author");
     },
     allAuthors: async () => Author.find({}),
+    me: async (root, args, context) => {
+      return context.currentUser;
+    },
   },
   Author: {
     bookCount: async (root) => {
@@ -71,7 +93,10 @@ const resolvers = {
     },
   },
   Mutation: {
-    addBook: async (root, args) => {
+    // destructure currentUser from context
+    addBook: async (root, args, { currentUser }) => {
+      if (!currentUser) throw new AuthenticationError("not authenticated");
+
       let existingAuthor = await Author.findOne({ name: args.author });
       if (!existingAuthor) {
         const newAuthor = new Author({ name: args.author });
@@ -93,7 +118,8 @@ const resolvers = {
       }
       return book;
     },
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, { currentUser }) => {
+      if (!currentUser) throw new AuthenticationError("not authenticated");
       const existingAuthor = await Author.findOne({ name: args.name });
       if (!existingAuthor) throw new UserInputError("author does not exist");
       existingAuthor.born = args.setBornTo;
@@ -106,12 +132,48 @@ const resolvers = {
       }
       return existingAuthor;
     },
+    createUser: async (root, args) => {
+      const user = new User({ ...args });
+      try {
+        await user.save();
+      } catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        });
+      }
+      return user;
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username });
+
+      // for this exercise all passes are password
+      // refer to part 4 for bcrypt hashing etc...
+      if (!user || args.password !== "password") {
+        throw new UserInputError("wrong credentials");
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, process.env.SECRET) };
+    },
   },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.toLowerCase().startsWith("bearer ")) {
+      const decodedToken = jwt.verify(auth.substring(7), process.env.SECRET);
+      // may need .populate here if User also has an object field User.findById(...).populate("friends")
+      const currentUser = await User.findById(decodedToken.id);
+      return { currentUser };
+    }
+  },
 });
 
 server.listen().then(({ url }) => {
