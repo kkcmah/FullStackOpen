48f4213d2f5ac458a237647ab810b8f0cb830373
Kelby Mah <kelbymah@gmail.com>
2022-04-15 15:49:40 -0600

    exercise 9.26 9.27 patientor step 11 - 12 use single form to handle the 3 different entry types and display validation errors

--- part9/patientor-main/src/PatientDetailPage/AddEntryForm.tsx
+++ part9/patientor-main/src/PatientDetailPage/AddEntryForm.tsx
@@ -1,5 +1,5 @@
 import { Button, Grid } from "@material-ui/core";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, FormikErrors } from "formik";
 import {
   DiagnosisSelection,
   SelectField,
@@ -20,11 +20,123 @@ const healthCheckRatingOptions = [
   { value: HealthCheckRating.CriticalRisk, label: "Critical Risk" },
 ];
 
+const entryTypeOptions = [
+  { value: EntryType.HealthCheck, label: "Health Check" },
+  { value: EntryType.Hospital, label: "Hospital" },
+  { value: EntryType.OccupationalHealthcare, label: "Occupational Healthcare" },
+];
+
+interface EntryFormErrors
+  extends Omit<FormikErrors<EntryFormValues>, "sickLeave" | "discharge"> {
+  sickLeave?: {
+    startDate?: string;
+    endDate?: string;
+  };
+  discharge?: {
+    date?: string;
+    criteria?: string;
+  };
+}
+
 const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
   const [{ diagnoses }] = useStateValue();
 
   const isValidDate = (date: string) => {
     return Boolean(Date.parse(date));
+  };
+
+  const requiredError = "Field is required";
+  const invalidDate = "Date is invalid. Format: YYYY-MM-DD";
+
+  const validateBase = (values: EntryFormValues, errors: EntryFormErrors) => {
+    if (!values.description) {
+      errors.description = requiredError;
+    }
+    if (!values.date) {
+      errors.date = requiredError;
+    }
+    if (!isValidDate(values.date)) {
+      errors.date = invalidDate;
+    }
+    if (!values.specialist) {
+      errors.specialist = requiredError;
+    }
+    return errors;
+  };
+
+  const validateHealthCheckForm = (
+    values: EntryFormValues,
+    errors: EntryFormErrors
+  ): EntryFormErrors => {
+    if (
+      values.healthCheckRating === null ||
+      values.healthCheckRating === undefined
+    ) {
+      errors.healthCheckRating = requiredError;
+    }
+    return errors;
+  };
+
+  const validateOccHealthcareForm = (
+    values: EntryFormValues,
+    errors: EntryFormErrors
+  ): EntryFormErrors => {
+    if (!values.employerName) {
+      errors.employerName = requiredError;
+    }
+
+    if (
+      values.sickLeave &&
+      values.sickLeave.startDate !== "" &&
+      !isValidDate(values.sickLeave.startDate)
+    ) {
+      errors.sickLeave = { ...errors.sickLeave, startDate: invalidDate };
+    }
+
+    if (
+      values.sickLeave &&
+      !values.sickLeave.startDate &&
+      values.sickLeave.endDate
+    ) {
+      errors.sickLeave = { ...errors.sickLeave, startDate: requiredError };
+    }
+
+    if (
+      values.sickLeave &&
+      values.sickLeave.endDate !== "" &&
+      !isValidDate(values.sickLeave.endDate)
+    ) {
+      errors.sickLeave = { ...errors.sickLeave, endDate: invalidDate };
+    }
+
+    if (
+      values.sickLeave &&
+      values.sickLeave.startDate &&
+      !values.sickLeave.endDate
+    ) {
+      errors.sickLeave = { ...errors.sickLeave, endDate: requiredError };
+    }
+
+    return errors;
+  };
+
+  const validateHospitalForm = (
+    values: EntryFormValues,
+    errors: EntryFormErrors
+  ): EntryFormErrors => {
+    if (!values.discharge) {
+      return errors;
+    }
+    if (!isValidDate(values.discharge.date)) {
+      errors.discharge = { ...errors.discharge, date: invalidDate };
+    }
+    if (!values.discharge.criteria) {
+      errors.discharge = {
+        ...errors.discharge,
+        criteria: "criteria is required",
+      };
+    }
+    return errors;
   };
 
   return (
@@ -35,32 +147,37 @@ const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
         date: "",
         specialist: "",
         diagnosisCodes: [],
+        // health check entry values
         healthCheckRating: 0,
+        // occupational health entry values
+        employerName: "",
+        sickLeave: { startDate: "", endDate: "" },
+        // hospital entry values
+        discharge: { date: "", criteria: "" },
       }}
       onSubmit={onSubmit}
       validate={(values) => {
-        const requiredError = "Field is required";
-        const invalidDate = "Date is invalid. Format: YYYY-MM-DD";
-        const errors: { [field: string]: string } = {};
-        if (!values.description) {
-          errors.description = requiredError;
-        }
-        if (!values.date) {
-          errors.date = requiredError;
-        }
-        if (!isValidDate(values.date)) {
-          errors.date = invalidDate;
-        }
-        if (!values.specialist) {
-          errors.specialist = requiredError;
+        let errors: EntryFormErrors = {};
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        errors = validateBase(values, errors);
+        if (values.type === EntryType.HealthCheck) {
+          return validateHealthCheckForm(values, errors);
+        } else if (values.type === EntryType.OccupationalHealthcare) {
+          return validateOccHealthcareForm(values, errors);
+        } else if (values.type === EntryType.Hospital) {
+          return validateHospitalForm(values, errors);
         }
         return errors;
       }}
     >
-      {({ isValid, dirty, setFieldValue, setFieldTouched }) => {
+      {({ isValid, dirty, setFieldValue, setFieldTouched, values }) => {
         return (
           <Form className="form ui">
-            <h3>Adding HealthCheck entry</h3>
+            <SelectField
+              label="Entry type"
+              name="type"
+              options={entryTypeOptions}
+            />
             <Field
               label="description"
               placeholder="description"
@@ -79,11 +196,54 @@ const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
               name="specialist"
               component={TextField}
             />
-            <SelectField
-              label="health check rating"
-              name="healthCheckRating"
-              options={healthCheckRatingOptions}
-            />
+
+            {values.type === EntryType.HealthCheck && (
+              <SelectField
+                label="health check rating"
+                name="healthCheckRating"
+                options={healthCheckRatingOptions}
+              />
+            )}
+
+            {values.type === EntryType.OccupationalHealthcare && (
+              <>
+                <Field
+                  label="employer name"
+                  placeholder="employer name"
+                  name="employerName"
+                  component={TextField}
+                />
+                <Field
+                  label="sick leave start date"
+                  placeholder="sick leave start date"
+                  name="sickLeave.startDate"
+                  component={TextField}
+                />
+                <Field
+                  label="sick leave end date"
+                  placeholder="sick leave end date"
+                  name="sickLeave.endDate"
+                  component={TextField}
+                />
+              </>
+            )}
+
+            {values.type === EntryType.Hospital && (
+              <>
+                <Field
+                  label="discharge date"
+                  placeholder="discharge date"
+                  name="discharge.date"
+                  component={TextField}
+                />
+                <Field
+                  label="criteria"
+                  placeholder="critera"
+                  name="discharge.criteria"
+                  component={TextField}
+                />
+              </>
+            )}
 
             <DiagnosisSelection
               setFieldValue={setFieldValue}
